abf1c6435b59d46fa331ab8b23d0b97cc5af9c44
Samuel Rourke <samuelvanrourke@gmail.com>
2021-12-19 22:17:58 -0500

    made seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+input_info = [
+  { input_type: "text", name: "name", label: "name" },
+  { input_type: "email", name: "email", label: "email" },
+  { input_type: "date", name: "vist_date", label: "When did you visit?" },
+  {
+    input_type: "select",
+    name: "reason_for_visit",
+    label: "Reason for visit",
+    survey_input_options_attributes: [
+      { value: "Haircut" },
+      { value: "Styling" },
+      { value: "Shave" },
+    ],
+  },
+  { input_type: "checkbox", name: "reccomendation", label: "Would you reccomend us to your friends?" },
+  { input_type: "textarea", name: "comments", label: "Do you have any comments?" },
+]
 User.create(name: "Sam")
-User.first.surveys.create(title: "Survey 1")
+survey = User.first.surveys.create(title: "Survey 1")
+input_info.each { |input| survey.survey_inputs.create(input) }
